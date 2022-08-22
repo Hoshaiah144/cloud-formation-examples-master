ad2858bbe34cf58f1e4c292ea8ca319491a137d8
Hoshaiah144 <108906554+Hoshaiah144@users.noreply.github.com>
2022-08-22 08:18:20 -0400

    Add files via upload

---
+++ cloudformation-examples-master/examples/README.md
@@ -0,0 +1,3 @@
+# Prereqs
+
+awscli
---
+++ cloudformation-examples-master/examples/cf-update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. cf-defaults.sh
+aws --region "${REGION}" cloudformation update-stack \
+	--stack-name "${STACK_NAME}" \
+	--template-body "${TEMPLATE_FILE}" \
+	--capabilities CAPABILITY_IAM
